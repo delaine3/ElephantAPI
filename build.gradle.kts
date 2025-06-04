5b44405e243808ab9e711b3dcb587b406bac4138
Delaine Abner <50344531+delaine3@users.noreply.github.com>
2025-06-04 01:44:14 -0400

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     id("org.springframework.boot") version "3.3.1"
     id("io.spring.dependency-management") version "1.1.5"
 }
+springBoot {
+    mainClass.set("org.example.ElephantApiApplication")
+}
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
