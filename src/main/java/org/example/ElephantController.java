e65e8931cdab03967700493a97caf3e21bbc7742
Delaine Abner <50344531+delaine3@users.noreply.github.com>
2025-06-04 08:39:55 -0400

    Rebuild and redeploy backend

--- src/main/java/org/example/ElephantController.java
+++ src/main/java/org/example/ElephantController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 })
 public class ElephantController {
 
-    @GetMapping("/")
+    @GetMapping("/") 
     public String home() {
         return "The backend is running! 🐘";
     }
